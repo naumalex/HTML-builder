d53f5b78913d782607c794355c167a86fb8d1fef
Aliaksandr Naumavets <naum1979alex@gmail.com>
2024-01-20 17:53:36 +0300

    feat: implement task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const getFiles = async function () {
+  const FOLDER_WITH_FILES = './03-files-in-folder/secret-folder';
+  try {
+    const files = await fs.readdir(FOLDER_WITH_FILES, {
+      withFileTypes: true,
+    });
+    files.forEach(async (file) => {
+      if (file.isFile()) {
+        let stat = await fs.stat(path.join(FOLDER_WITH_FILES, file.name));
+        console.log(
+          `${path.basename(file.name)} - ${path
+            .extname(file.name)
+            .replace('.', '')} - ${stat.size / 1000}kb`,
+        );
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getFiles();
