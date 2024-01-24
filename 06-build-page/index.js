5cbf97c2132580ffbd8822bf61b5acb0947a7477
Aliaksandr Naumavets <naum1979alex@gmail.com>
2024-01-24 14:29:23 +0300

    fix: no error if tags separated by space only

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,7 +6,7 @@ const copyDirectory = require('../04-copy-directory/index.js');
 const buildPage = async function() {
   const PARENT_DIRECTORY = './06-build-page';
   let template = await fs.readFile(path.join(PARENT_DIRECTORY, 'template.html'), 'utf-8');
-  let tags = template.match(/{{.*}}/g);
+  let tags = template.match(/{{[^{]*}}/g);
   for (let tag of tags) {
     const component = await fs.readFile(path.join(PARENT_DIRECTORY, 'components', 
       `${tag.substring(2, tag.length - 2)}.html`));
